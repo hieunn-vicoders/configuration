5c8ad12af83b205bf31ab3ac38439f07ad7489e7
Nguyen Nhat Linh <linhnn@vmms.vn>
2021-01-20 08:13:53 +0700

    Update: Update function and config System Option

--- src/app/Helpers/GetOption.php
+++ src/app/Helpers/GetOption.php
@@ -34,3 +34,206 @@ if (!function_exists('prepareOption')) {
     }
 }
 
+function get_args()
+{
+    $args   = func_get_args();
+    $string = '-';
+    foreach ($args as $arg) {
+        $string .= $arg . '-';
+    }
+    $key = rtrim($string, '-');
+    return $key;
+}
+
+if (!function_exists('get_logo_website')) {
+    function get_logo_website($type = null, $size = null, $transparent = null)
+    {
+        $key = get_args($type, $size, $transparent);
+        return OptionService::get('logo-website' . $key);
+    }
+}
+
+if (!function_exists('get_slide')) {
+    function get_slide($param, $type = null, $size = null, $transparent = null)
+    {
+        $key = get_args($param, $type, $size, $transparent);
+        return OptionService::get('slide' . $key);
+    }
+}
+
+if (!function_exists('get_logo_partner')) {
+    function get_logo_partner($param, $type, $size, $transparent)
+    {
+        $key = get_args($param, $type, $size, $transparent);
+        return OptionService::get('logo-partner' . $key);
+    }
+}
+if (!function_exists('get_banner')) {
+    function get_banner($param, $type, $size, $transparent)
+    {
+        $key = get_args($param, $type, $size, $transparent);
+        return OptionService::get('banner' . $key);
+    }
+}
+
+if (!function_exists('get_infor_name')) {
+    function get_infor_name()
+    {
+        return OptionService::get('infor-name');
+    }
+}
+
+if (!function_exists('get_infor_email')) {
+    function get_infor_email()
+    {
+        return OptionService::get('infor-email');
+    }
+}
+
+if (!function_exists('get_infor_hotline')) {
+    function get_infor_hotline()
+    {
+        return OptionService::get('infor-hotline');
+    }
+}
+
+if (!function_exists('get_infor_fax')) {
+    function get_infor_fax()
+    {
+        return OptionService::get('infor-fax');
+    }
+}
+
+if (!function_exists('get_infor_support_online')) {
+    function get_infor_support_online()
+    {
+        return OptionService::get('infor-support-online');
+    }
+}
+
+if (!function_exists('get_infor_address')) {
+    function get_infor_address()
+    {
+        return OptionService::get('infor-address');
+    }
+}
+
+if (!function_exists('get_infor_facebook')) {
+    function get_infor_facebook()
+    {
+        return OptionService::get('infor-facebook');
+    }
+}
+
+if (!function_exists('get_infor_instagram')) {
+    function get_infor_instagram()
+    {
+        return OptionService::get('infor-instagram');
+    }
+}
+
+if (!function_exists('get_infor_twitter')) {
+    function get_infor_twitter()
+    {
+        return OptionService::get('infor-twitter');
+    }
+}
+
+if (!function_exists('get_infor_zalo')) {
+    function get_infor_zalo()
+    {
+        return OptionService::get('infor-zalo');
+    }
+}
+
+if (!function_exists('get_infor_youtube')) {
+    function get_infor_youtube()
+    {
+        return OptionService::get('infor-youtube');
+    }
+}
+
+if (!function_exists('get_infor_fanpage')) {
+    function get_infor_fanpage()
+    {
+        return OptionService::get('infor-fanpage');
+    }
+}
+
+if (!function_exists('get_infor_map')) {
+    function get_infor_map()
+    {
+        return OptionService::get('infor-map');
+    }
+}
+
+if (!function_exists('get_icon_service')) {
+    function get_icon_service($param, $type, $size, $transparent)
+    {
+        $key = get_args($param, $type, $size, $transparent);
+        return OptionService::get('icon-service' . $key);
+    }
+}
+
+if (!function_exists('get_title_service')) {
+    function get_title_service($param)
+    {
+        return OptionService::get('title-service' . $param);
+    }
+}
+
+if (!function_exists('get_description_service')) {
+    function get_description_service($param)
+    {
+        return OptionService::get('description-service-' . $param);
+    }
+}
+
+if (!function_exists('get_title')) {
+    function get_title($page)
+    {
+        return OptionService::get('title-' . $page);
+    }
+}
+
+if (!function_exists('get_description')) {
+    function get_description($page)
+    {
+        return OptionService::get('description-' . $page);
+    }
+}
+
+if (!function_exists('get_favicon')) {
+    function get_favicon()
+    {
+        return OptionService::get('favicon');
+    }
+}
+
+if (!function_exists('get_sologan')) {
+    function get_slogogan()
+    {
+        return OptionService::get('sologan');
+    }
+}
+
+if (!function_exists('get_contact_me')) {
+    function get_contact_me()
+    {
+        return OptionService::get('contact-me');
+    }
+}
+
+if (!function_exists('get_operation_license')) {
+    function get_operation_license()
+    {
+        return OptionService::get('operation-license');
+    }
+}
+
+if (!function_exists('get_copy_by')) {
+    function get_copy_by()
+    {
+        return OptionService::get('copy-by');
+    }
+}
--- src/config/configuration.php
+++ src/config/configuration.php
@@ -1,5 +1,272 @@
 <?php
 
 return [
-
+    [
+        'label'  => 'Cài đặt chung',
+        'inputs' => [
+            [
+                'label' => 'Header Logo',
+                'key'   => 'Logo-website',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Favicon',
+                'key'   => 'favicon',
+                'note'  => 'Hướng dẫn tạo favicon: <a href="https://www.favicon-generator.org/" target="_blank">Tại đây</a>',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Footer Logo',
+                'key'   => 'logo-website-small',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Sologan Website',
+                'key'   => 'sologan',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Hãy liên hệ với chúng tôi',
+                'key'   => 'contact-me',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Tên công ty/cửa hàng',
+                'key'   => 'infor-name',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Địa chỉ công ty/cửa hàng',
+                'key'   => 'infor-address',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Hotline',
+                'key'   => 'infor-hotline',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Hỗ trợ trực tuyến',
+                'key'   => 'infor-support-online',
+                'type'  => 'textarea',
+            ],
+            [
+                'label' => 'Link Facebook',
+                'key'   => 'infor-facebook',
+                'type'  => 'text',
+            ],
+            [
+                'label' => ' Link Twitter',
+                'key'   => 'infor-twitter',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Link Instagram',
+                'key'   => 'infor-instagram',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Fanpage facebook',
+                'key'   => 'infor-fanpage',
+                'note'  => 'Hướng dẫn get fanpage facebook plugin: <a href="https://developers.facebook.com/docs/plugins/page-plugin" target="_blank">Tại đây</a>',
+                'type'  => 'textarea',
+            ],
+            [
+                'label' => 'Bản đồ chỉ đường - Google map',
+                'key'   => 'infor-map',
+                'note'  => 'Hướng dẫn get google map plugin: <a href="https://support.google.com/maps/answer/144361?co=GENIE.Platform%3DDesktop&hl=en" target="_blank">Tại đây</a>',
+                'type'  => 'textarea',
+            ],
+            [
+                'label' => 'Số Fax',
+                'key'   => 'infor-fax',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Copyright by',
+                'key'   => 'copy-by',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Giấy phép hoạt động',
+                'key'   => 'operation-license',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Trang chủ',
+        'inputs' => [
+            [
+                'label' => 'Trang chủ title',
+                'key'   => 'title-trang-chu',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Trang chủ description',
+                'key'   => 'description-trang-chu',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'slide 1',
+                'key'   => 'slide-1',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'slide 2',
+                'key'   => 'slide-2',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'slide 3',
+                'key'   => 'slide-3',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'slide 4',
+                'key'   => 'slide-4',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'slide 5',
+                'key'   => 'slide-5',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Icon dịch vụ 1',
+                'key'   => 'icon-service-1',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Title dịch vụ 1',
+                'key'   => 'title-service-1',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Description dịch vụ 1',
+                'key'   => 'description-service-1',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Icon dịch vụ 2',
+                'key'   => 'icon-service-2',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Title dịch vụ 2',
+                'key'   => 'title-service-2',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Description dịch vụ 2',
+                'key'   => 'description-service-2',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Icon dịch vụ 3',
+                'key'   => 'icon-service-3',
+                'type'  => 'image',
+            ],
+            [
+                'label' => 'Title dịch vụ 3',
+                'key'   => 'title-service-2',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Description dịch vụ 3',
+                'key'   => 'description-service-2',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Giới thiệu',
+        'inputs' => [
+            [
+                'label' => 'Giới thiệu title',
+                'key'   => 'title-gioi-thieu',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Giới thiệu description',
+                'key'   => 'description-gioi-thieu',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Sản phẩm',
+        'inputs' => [
+            [
+                'label' => 'Sản phẩm title',
+                'key'   => 'title-san-pham',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Sản phẩm description',
+                'key'   => 'description-san-pham',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Dịch vụ',
+        'inputs' => [
+            [
+                'label' => 'Dịch vụ title',
+                'key'   => 'title-dich-vu',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Dịch vụ description',
+                'key'   => 'description-dich-vu',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Tin tức',
+        'inputs' => [
+            [
+                'label' => 'Tin tức title',
+                'key'   => 'title-tin-tuc',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Tin tức description',
+                'key'   => 'description-tin-tuc',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Liên hệ',
+        'inputs' => [
+            [
+                'label' => 'Liên hệ title',
+                'key'   => 'title-lien-he',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Liên hệ description',
+                'key'   => 'description-lien-he',
+                'type'  => 'text',
+            ],
+        ],
+    ],
+    [
+        'label'  => 'Giỏ hàng',
+        'inputs' => [
+            [
+                'label' => 'Giỏ hàng title',
+                'key'   => 'title-gio-hang',
+                'type'  => 'text',
+            ],
+            [
+                'label' => 'Giỏ hàng description',
+                'key'   => 'description-gio-hang',
+                'type'  => 'text',
+            ],
+        ],
+    ],
 ];
