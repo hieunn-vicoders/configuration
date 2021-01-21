6d4fa987231fd1e7ae35d251c21d0e7fe15b6767
Nguyen Nhat Linh <linhnn@vmms.vn>
2021-01-21 17:14:11 +0700

    Update: add param for each function

--- src/app/Helpers/GetOption.php
+++ src/app/Helpers/GetOption.php
@@ -61,6 +61,13 @@ if (!function_exists('get_slide')) {
     }
 }
 
+if (!function_exists('get_slide_content')) {
+    function get_slide_content($param)
+    {
+        return OptionService::get('slide-content-' . $param);
+    }
+}
+
 if (!function_exists('get_logo_partner')) {
     function get_logo_partner($param, $type = null, $size = null, $transparent = null)
     {
@@ -77,100 +84,142 @@ if (!function_exists('get_banner')) {
 }
 
 if (!function_exists('get_info_name')) {
-    function get_info_name()
+    function get_info_name($param = null)
     {
-        return OptionService::get('info-name');
+        if ($param === null) {
+            return OptionService::get('info-name');
+        }
+        return OptionService::get('info-name-' . $param);
     }
 }
 
 if (!function_exists('get_info_description')) {
-    function get_info_description()
+    function get_info_description($param = null)
     {
-        return OptionService::get('info-description');
+        if ($param === null) {
+            return OptionService::get('info-description');
+        }
+        return OptionService::get('info-description-' . $param);
     }
 }
 
 if (!function_exists('get_info_email')) {
-    function get_info_email()
+    function get_info_email($param = null)
     {
-        return OptionService::get('info-email');
+        if ($param === null) {
+            return OptionService::get('info-email');
+        }
+        return OptionService::get('info-email-' . $param);
     }
 }
 
 if (!function_exists('get_info_hotline')) {
-    function get_info_hotline()
+    function get_info_hotline($param = null)
     {
-        return OptionService::get('info-hotline');
+        if ($param === null) {
+            return OptionService::get('info-hotline');
+        }
+        return OptionService::get('info-hotline-' . $param);
     }
 }
 
 if (!function_exists('get_info_fax')) {
-    function get_info_fax()
+    function get_info_fax($param = null)
     {
-        return OptionService::get('info-fax');
+        if ($param === null) {
+            return OptionService::get('info-fax');
+        }
+        return OptionService::get('info-fax-' . $param);
     }
 }
 
 if (!function_exists('get_info_support_online')) {
-    function get_info_support_online()
+    function get_info_support_online($param)
     {
-        return OptionService::get('info-support-online');
+        if ($param === null) {
+            return OptionService::get('info-support-online');
+        }
+        return OptionService::get('info-support-online-' . $param);
     }
 }
 
 if (!function_exists('get_info_address')) {
-    function get_info_address()
+    function get_info_address($param = null)
     {
-        return OptionService::get('info-address');
+        if ($param === null) {
+            return OptionService::get('info-address');
+        }
+        return OptionService::get('info-address-' . $param);
     }
 }
 
 if (!function_exists('get_info_facebook')) {
-    function get_info_facebook()
+    function get_info_facebook($param = null)
     {
-        return OptionService::get('info-facebook');
+        if ($param === null) {
+            return OptionService::get('info-facebook');
+        }
+        return OptionService::get('info-facebook-' . $param);
     }
 }
 
 if (!function_exists('get_info_instagram')) {
-    function get_info_instagram()
+    function get_info_instagram($param = null)
     {
-        return OptionService::get('info-instagram');
+        if ($param === null) {
+            return OptionService::get('info-instagram');
+        }
+        return OptionService::get('info-instagram-' . $param);
     }
 }
 
 if (!function_exists('get_info_twitter')) {
-    function get_info_twitter()
+    function get_info_twitter($param = null)
     {
-        return OptionService::get('info-twitter');
+        if ($param === null) {
+            return OptionService::get('info-twitter');
+        }
+        return OptionService::get('info-twitter-' . $param);
     }
 }
 
 if (!function_exists('get_info_zalo')) {
-    function get_info_zalo()
+    function get_info_zalo($param = null)
     {
-        return OptionService::get('info-zalo');
+        if ($param === null) {
+            return OptionService::get('info-zalo');
+        }
+        return OptionService::get('info-zalo-' . $param);
     }
 }
 
 if (!function_exists('get_info_youtube')) {
-    function get_info_youtube()
+    function get_info_youtube($param = null)
     {
-        return OptionService::get('info-youtube');
+        if ($param === null) {
+            return OptionService::get('info-youtube');
+        }
+        return OptionService::get('info-youtube-' . $param);
     }
 }
 
 if (!function_exists('get_info_fanpage')) {
-    function get_info_fanpage()
+    function get_info_fanpage($param = null)
     {
-        return OptionService::get('info-fanpage');
+        if ($param === null) {
+            return OptionService::get('info-fanpage');
+        }
+        return OptionService::get('info-fanpage-' . $param);
     }
 }
 
 if (!function_exists('get_info_map')) {
-    function get_info_map()
+    function get_info_map($param)
     {
-        return OptionService::get('info-map');
+        if ($param === null) {
+            return OptionService::get('info-map');
+        }
+        return OptionService::get('info-map-' . $param);
     }
 }
 
@@ -197,15 +246,21 @@ if (!function_exists('get_description_service')) {
 }
 
 if (!function_exists('get_title')) {
-    function get_title($page)
+    function get_title($page = null)
     {
+        if ($page === null) {
+            return OptionService::get('title-website');
+        }
         return OptionService::get('title-' . $page);
     }
 }
 
 if (!function_exists('get_description')) {
-    function get_description($page)
+    function get_description($page = null)
     {
+        if ($page === null) {
+            return OptionService::get('description-website');
+        }
         return OptionService::get('description-' . $page);
     }
 }
@@ -218,29 +273,41 @@ if (!function_exists('get_favicon')) {
 }
 
 if (!function_exists('get_sologan')) {
-    function get_sologan()
+    function get_sologan($param = null)
     {
-        return OptionService::get('sologan');
+        if ($page === null) {
+            return OptionService::get('sologan');
+        }
+        return OptionService::get('sologan-' . $param);
     }
 }
 
 if (!function_exists('get_contact_me')) {
-    function get_contact_me()
+    function get_contact_me($param = null)
     {
-        return OptionService::get('contact-me');
+        if ($param === null) {
+            return OptionService::get('contact-me');
+        }
+        return OptionService::get('contact-me-' . $param);
     }
 }
 
 if (!function_exists('get_operation_license')) {
-    function get_operation_license()
+    function get_operation_license($param = null)
     {
-        return OptionService::get('operation-license');
+        if ($param === null) {
+            return OptionService::get('operation-license');
+        }
+        return OptionService::get('operation-license-' . $param);
     }
 }
 
 if (!function_exists('get_copy_by')) {
-    function get_copy_by()
+    function get_copy_by($param = null)
     {
-        return OptionService::get('copy-by');
+        if ($param === null) {
+            return OptionService::get('copy-by');
+        }
+        return OptionService::get('copy-by-' . $param);
     }
 }
